0261a715b5318d68b2f0c6f574bc467118d142fd
myscosta8-ctrl <myscosta8@gmail.com>
2026-08-08 08:50:11 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 //  O cache serve só para o app continuar abrindo sem internet.
 // ============================================================
 
-const CACHE = "farmafamilia-v14";
+const CACHE = "farmafamilia-v15";
 const ARQUIVOS = [
   "./",
   "./index.html",
